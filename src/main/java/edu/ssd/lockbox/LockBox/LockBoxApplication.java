fa72aea45e11d9c678f0c2c0b28072b9d3553457
Dineth_112221 <manuja.gayan@axiatadigitallabs.com>
2022-11-05 16:45:00 +0530

    first commit

---
+++ src/main/java/edu/ssd/lockbox/LockBox/LockBoxApplication.java
@@ -0,0 +1,13 @@
+package edu.ssd.lockbox.LockBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ssd/lockbox/LockBox/LockBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ssd.lockbox.LockBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
